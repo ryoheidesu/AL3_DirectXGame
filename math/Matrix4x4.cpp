1272018f785df071c03c1dddaebf47786339dcfd
ryoheidesu <k022g1048@g.neec.ac.jp>
2023-04-28 14:19:16 +0900

    移動処理

--- math/Matrix4x4.cpp
+++ math/Matrix4x4.cpp
@@ -24,7 +24,6 @@ Matrix4x4 MakeRotateXMatrix(float theta)
 	0.0f,cos,sin,0.0f,
 	0.0f,-sin,cos,0.0f,
 	0.0f,0.0f,0.0f,1.0f
-
 	};
 	return result;
 
@@ -41,7 +40,6 @@ Matrix4x4 MakeRotateYMatrix(float theta) {
 	0.0f,1.0f,0.0f,0.0f,
 	sin,0.0f,cos,0.0f,
 	0.0f, 0.0f, 0.0f,1.0f
-
 	};
 	return result;
 
@@ -58,20 +56,53 @@ Matrix4x4 MakeRotateZMatrix(float theta) {
 	-sin,cos,0.0f,0.0f,
 	0.0f,0.0f,1.0f,0.0f,
 	0.0f,0.0f,0.0f,1.0f
-
 	};
 	return result;
 }
 
-
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate) { return Matrix4x4(); }
+Matrix4x4 MakeTranslateMatrix(Vector3 translate) {
+	Matrix4x4 result;
+	result = {
+	1.0f,0.0f,0.0f,0.0f,
+	0.0f,1.0f,0.0f,0.0f,
+	0.0f,0.0f,1.0f,0.0f,
+	translate.x,translate.y,translate.z,1.0f
+	};
+	return result;
+}
 
 Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vector3& translate) {
-	return Matrix4x4();
+	Matrix4x4 matScale = MakeScaleMatrix(scale);
+
+	Matrix4x4 matRotX = MakeRotateXMatrix(rot.x);
+	Matrix4x4 matRotY = MakeRotateXMatrix(rot.y);
+	Matrix4x4 matRotZ = MakeRotateXMatrix(rot.z);
+
+	Matrix4x4 matRot = matRotZ * matRotX * matRotY;
+
+	Matrix4x4 matTrans = MakeTranslateMatrix(translate);
+
+	Matrix4x4 result = matScale * matRot * matTrans;
+
+	return result;
 }
 
 Matrix4x4& operator*=(Matrix4x4& m1, const Matrix4x4& m2) {
-	// TODO: return ステートメントをここに挿入します
+	Matrix4x4 result = {};
+	for (size_t i = 0; i < 4; i++) {
+		for (rsize_t j = 0; j < 4; j++) {
+			for (rsize_t k = 0; k < 4; k++) {
+				result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+			}
+		}
+	}
+
+	m1 = result;
+	return m1;
 }
 
-Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2) { return Matrix4x4(); }
+Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result = m1;
+
+	return result *= m2;
+}
