60dff03a597f643aa34c653799366491f96efbc2
ryoheidesu <k022g1048@g.neec.ac.jp>
2023-05-26 14:52:59 +0900

    EnemyBullet追加

---
+++ EnemyBullet.cpp
@@ -0,0 +1,27 @@
+//#include "EnemyBullet.h"
+//#include <cassert>
+//
+//void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
+//
+//	assert(model);
+//	model_ = model;
+//	velocity_ = velocity;
+//	textureHandle_ = TextureManager::Load("/cube/cube.jpg");
+//
+//	worldTransform_.Initialize();
+//	worldTransform_.translation_.x = position.x;
+//	worldTransform_.translation_.y = position.y;
+//	worldTransform_.translation_.z = position.z;
+//}
+//
+//void EnemyBullet::Update() {
+//
+//	worldTransform_.translation_.x += velocity_.x;
+//	worldTransform_.translation_.y += velocity_.y;
+//	worldTransform_.translation_.z += velocity_.z;
+//	worldTransform_.UpdateMatrix();
+//}
+//
+//void EnemyBullet::Draw(const ViewProjection& viewProjection) {
+//	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+//}
---
+++ EnemyBullet.h
@@ -0,0 +1,19 @@
+//#pragma once
+//#include "Model.h"
+//#include "WorldTransform.h"
+//
+//class EnemyBullet {
+//
+//public:
+//	void Initialize(Model* model, const Vector3& position, const Vector3& velocity);
+//
+//	void Update();
+//
+//	void Draw(const ViewProjection& viewProjection);
+//
+//private:
+//	WorldTransform worldTransform_;
+//	Model* model_ = nullptr;
+//	uint32_t textureHandle_ = 0u;
+//	Vector3 velocity_;
+//};
