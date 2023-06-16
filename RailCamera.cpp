d519247046ff8ebb00bf8c98f16cc04539a44450
ryoheidesu <k022g1048@g.neec.ac.jp>
2023-06-16 14:26:50 +0900

    test

--- RailCamera.cpp
+++ RailCamera.cpp
@@ -1,11 +1,11 @@
-//#include "RailCamera.h"
-//
-//void RailCamera::Initialize(Vector3 trans, Vector3 rot) {
-//	worldTransform_.Initialize();
-//	
-//
-//}
-//
-//void RailCamera::Update() {
-//
-//}
+#include "RailCamera.h"
+
+void RailCamera::Initialize(Vector3 trans, Vector3 rot) {
+	worldTransform_.Initialize();
+	
+
+}
+
+void RailCamera::Update() {
+
+}
